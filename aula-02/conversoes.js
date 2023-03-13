59122e40b10d152e1923396c40985ec7f1a8c311
armanoalves <armanobarrosalvesh@gmail.com>
2023-03-13 14:24:48 -0300

    Arquivos da aula 02

---
+++ aula-02/conversoes.js
@@ -0,0 +1,21 @@
+// tipo de dado
+// booleanos
+
+// conversão implícita
+const numero = 456;
+const numeroString = Number("456a");
+
+// Number()
+// String()
+console.log(numero + numeroString);
+
+// conversão explícita
+
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+
+}
+console.log(respostaDeTudo, idade, pi)
