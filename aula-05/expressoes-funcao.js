30ac6a95fd1006f79d1a00a29b576a1748bb4e94
armanoalves <armanobarrosalvesh@gmail.com>
2023-03-13 14:25:18 -0300

    Arquivos da aula 05

---
+++ aula-05/expressoes-funcao.js
@@ -0,0 +1,22 @@
+function minhaFuncao(param) {
+    // Bloco de código
+}
+
+minhaFuncao("Param");
+
+// Expressão de função
+
+// const soma = function (num1, num2) { return num1 + num2}
+// console.log(soma(1, 1));
+
+// diferença principal ?????
+// funções e var são "listadas" no topo do arquivo.
+
+console.log(apresentacao());
+
+function apresentacao () {
+    return "olá";
+}
+
+console.log(soma(1,1));
+const soma = function (num1, num2) { return num1 + num2}
---
+++ aula-05/funcoes.js
@@ -0,0 +1,26 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// DECLARAÇÃO DE FUNÇÃO
+
+// 1) Declara a função
+
+function imprimeTexto(texto) {
+    console.log(texto);
+}
+
+// 2) Executa a função (1 ou + vezes)
+
+imprimeTexto(soma());
+// imprimeTexto("Outro texto");
+
+// Três formas de escrever funções
+
+function soma() {
+    // outros códigos
+    //vários console.log()
+    return 2 + 2;
+}
+
+// console.log(soma());
